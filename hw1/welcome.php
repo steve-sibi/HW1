f48467cc79aeadfec142621b4f7a26d2cb696774
Steve Sibi <steve.sibi@gmail.com>
2020-02-29 10:31:16 +0400

    new updates

--- hw1/welcome.php
+++ hw1/welcome.php
@@ -22,7 +22,7 @@ if (empty($_SESSION['userlogin']) || $_SESSION['userlogin'] = ''){
 }
 ?>
 <nav class="navbar navbar-light navbar-expand-lg fixed-top bg-white clean-navbar">
-    <div class="container"><a class="navbar-brand logo" href="#">Brand</a><button data-toggle="collapse" class="navbar-toggler" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+    <div class="container"><a class="navbar-brand logo" href="#">Welcome Admin</a><button data-toggle="collapse" class="navbar-toggler" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
         <div class="collapse navbar-collapse"
              id="navcol-1">
             <ul class="nav navbar-nav ml-auto">
