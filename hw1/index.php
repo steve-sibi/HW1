1dc031b9644bd433a51baf87be5fe31926eba3ef
Steve Sibi <steve.sibi@gmail.com>
2020-02-29 12:59:44 +0400

    new updates

--- hw1/index.php
+++ hw1/index.php
@@ -15,7 +15,7 @@
 <?php
 
 //session_start();
-
+//
 //$_SESSION['userlogin'] = "Loggedin";
 $usernameErr = $passwordErr = "";
 
@@ -23,29 +23,26 @@ $username = "admin";
 $password = "admin";
 # check to see if git works
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    if (isset($_POST['username']))
-    {
-        session_start();
-        $_SESSION['userlogin'] = $_POST['username'];
-        header('Location: welcome.php');
-    }
-    else{
-        if (empty($_POST["username"])) {
-            $usernameErr = "username is required";
+//    session_start();
+    if (empty($_POST["username"])) {
+        $usernameErr = "username is required";
+    } else {
+        if ($_POST["username"] != $username) {
+            $usernameErr = "Wrong username";
         } else {
-            if ($_POST["username"] != $username) {
-                $usernameErr = "Wrong username";
+            if (empty($_POST["password"])) {
+                $passwordErr = "password is required";
             } else {
-                if (empty($_POST["password"])) {
-                    $passwordErr = "password is required";
+                if ($_POST["password"] != $password) {
+                    $passwordErr = "Wrong password";
                 } else {
-                    if ($_POST["password"] != $password) {
-                        $passwordErr = "Wrong password";
-                    }
+                    $_SESSION['userlogin'] = "Loggedin";
+                    header('Location: welcome.php');
                 }
             }
         }
     }
+
 
 }
 
@@ -56,14 +53,17 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         <h2 class="sr-only">Login Form</h2>
         <div class="illustration"><i class="icon ion-ios-locked-outline"></i></div>
         <div class="form-group">
-            <input class="form-control" type="text" name="username" placeholder="username" >
-            <span class="error">* <?php echo $usernameErr;?></span>
+            <input class="form-control" type="text" name="username" placeholder="username">
+            <span class="error">* <?php echo $usernameErr; ?></span>
         </div>
         <div class="form-group">
-            <input class="form-control" type="password" name="password" placeholder="Password" >
-            <span class="error">* <?php echo $passwordErr;?></span>
+            <input class="form-control" type="password" name="password" placeholder="Password">
+            <span class="error">* <?php echo $passwordErr; ?></span>
         </div>
-        <div class="form-group"><button class="btn btn-primary btn-block" type="submit">Log In</button></div><a class="forgot" href="#">Forgot your email or password?</a></form>
+        <div class="form-group">
+            <button class="btn btn-primary btn-block" type="submit">Log In</button>
+        </div>
+        <a class="forgot" href="#">Forgot your email or password?</a></form>
 </div>
 <script src="assets/js/jquery.min.js"></script>
 <script src="assets/bootstrap/js/bootstrap.min.js"></script>
